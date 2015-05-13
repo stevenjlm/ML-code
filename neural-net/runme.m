f93ad966f31a2514287c436a1ec252861d177498
Steven Munn <sjmunn@klamath.cs.ucsb.edu>
2015-05-13 16:16:51 -0700

    Initialization for variables

--- neural-net/runme.m
+++ neural-net/runme.m
@@ -1,10 +1,60 @@
 % 
 %     ECE 594E HW 4
 %           Neural Network
-%     Main
+%     Main running script
+%     Parameters: nLayers  -> number of neural network layers
+%                          -> including in and out layers
+%                 nNeurons -> nLayers by 1 vector with # of neurons in
+%                          -> each layer
+%                 
 %
 %     Steven Munn
 %           from https://github.com/stevenjlm/ML-code
 
 clear; clc;
+
+% Parameters
+nLayers=3;
+nNeurons=[2;8;1];
+backPropSteps=30;
+
+% Initialization ------------------------
+
+if ~isequal([nLayers,1],size(nNeurons))
+    error('nNeurons should be an nLayers by 1 column vector');
+end
+
+% Weights
+W=cell(1,nLayers-1);
+for iLayer=1:(nLayers-1)
+    W{iLayer}=rand(nNeurons(iLayer+1),nNeurons(iLayer));
+end
+
+% Biases
+b=cell(1,nLayers-1);
+for iLayer=1:(nLayers-1)
+    b{iLayer}=rand(nNeurons(iLayer+1),1);
+end
+
+% z
+z=cell(1,nLayers);
+for iLayer=1:(nLayers)
+    b{iLayer}=zeros(nNeurons(iLayer),1);
+end
+
+% a
+a=cell(1,nLayers);
+for iLayer=1:(nLayers)
+    a{iLayer}=zeros(nNeurons(iLayer),1);
+end
+
+% Gradients
+delW=cell(1,nLayers-1);
+for iLayer=1:(nLayers-1)
+    W{iLayer}=zeros(nNeurons(iLayer+1),nNeurons(iLayer));
+end
+delb=cell(1,nLayers-1);
+for iLayer=1:(nLayers-1)
+    b{iLayer}=zeros(nNeurons(iLayer+1),1);
+end
 
--- neural-net/sigmoid.m
+++ neural-net/sigmoid.m
@@ -1,10 +1,11 @@
 % 
 %     ECE 594E HW 4
 %           Neural Network
-%     Main
-%     In:  z  -> n by m Matrix or scalar
+%     Sigmoid function
 %
-%     Out: fz -> n by m Matrix or scalar
+%     In:       z  -> n by m Matrix or scalar
+%
+%     Out:      fz -> n by m Matrix or scalar
 
 function fz = sigmoid(z)
 fz=1./(1+exp(-z));
