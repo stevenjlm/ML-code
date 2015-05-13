87c4d1c19f01126dbbb66d65bcb5ea45735c16c4
Steven Munn <sjmunn@klamath.cs.ucsb.edu>
2015-05-13 15:06:34 -0700

    began NN

---
+++ neural-net/runme.m
@@ -0,0 +1,10 @@
+% 
+%     ECE 594E HW 4
+%           Neural Network
+%     Main
+%
+%     Steven Munn
+%           from https://github.com/stevenjlm/ML-code
+
+clear; clc;
+
---
+++ neural-net/testing.m
@@ -0,0 +1,18 @@
+% 
+%     ECE 594E HW 4
+%           Neural Network
+%     Test Cases
+%
+%     Steven Munn
+%           from https://github.com/stevenjlm/ML-code
+
+%% Test sigmoid
+
+a=rand(4,3);
+b=sigmoid(a);
+disp('Expect 4 by 3 matrix');
+disp(b);
+
+%Checked
+
+%% 
